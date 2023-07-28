f4696b85afad38647f45e9cc19ae8f37acded9b2
hunwangchao <74533885+hunwangchao@users.noreply.github.com>
2023-07-28 17:27:42 +0800

    Update MainActivity.java

--- app/src/main/java/com/yinge/opengl/egl/MainActivity.java
+++ app/src/main/java/com/yinge/opengl/egl/MainActivity.java
@@ -1,91 +1,38 @@
-package com.yinge.opengl.egl;
+plugins {
+    id 'com.android.application'
+}
 
-import android.opengl.EGL14;
-import android.opengl.EGLConfig;
-import android.opengl.EGLContext;
-import android.opengl.EGLDisplay;
-import android.opengl.EGLSurface;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
+android {
+    namespace 'com.example.qtiegl14'
+    compileSdk 33
 
-import com.yinge.opengl.dev.R;
+    defaultConfig {
+        applicationId "com.example.qtiegl14"
+        minSdk 33
+        targetSdk 33
+        versionCode 1
+        versionName "1.0"
 
-public class MainActivity extends AppCompatActivity {
-    private EGLDisplay eglDisplay;
-    private EGLConfig eglConfig;
-    private EGLContext eglContext;
-    private EGLSurface eglSurface;
-    private TextView init;
-    private TextView release;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qctest);
-        init = findViewById(R.id.init);
-        release = findViewById(R.id.release);
-
-        init.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "init", Toast.LENGTH_SHORT).show();
-                init();
-            }
-        });
-
-        release.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "release", Toast.LENGTH_SHORT).show();
-                release();
-            }
-        });
-    }
-   //  EGL initial
-    private void init() {
-        eglDisplay = EGL14.eglGetDisplay(EGL14.EGL_DEFAULT_DISPLAY);
-        int[] version = new int[2];
-        EGL14.eglInitialize(eglDisplay, version, 0, version, 1);
-
-        int[] configAttributes = {
-                EGL14.EGL_RENDERABLE_TYPE, EGL14.EGL_OPENGL_ES2_BIT,
-                EGL14.EGL_RED_SIZE, 8,
-                EGL14.EGL_GREEN_SIZE, 8,
-                EGL14.EGL_BLUE_SIZE, 8,
-                EGL14.EGL_ALPHA_SIZE, 8,
-                EGL14.EGL_NONE
-        };
-        EGLConfig[] configs = new EGLConfig[1];
-        int[] numConfigs = new int[1];
-        EGL14.eglChooseConfig(eglDisplay, configAttributes, 0, configs, 0, 1, numConfigs, 0);
-        eglConfig = configs[0];
-
-        int[] contextAttributes = {
-                EGL14.EGL_CONTEXT_CLIENT_VERSION, 2,
-                EGL14.EGL_NONE
-        };
-
-        int[] surfaceAttributes = {
-                EGL14.EGL_WIDTH, 1,
-                EGL14.EGL_HEIGHT, 1,
-                EGL14.EGL_NONE
-        };
-
-        eglContext = EGL14.eglCreateContext(eglDisplay, eglConfig, EGL14.EGL_NO_CONTEXT, contextAttributes, 0);
-
-        eglSurface = EGL14.eglCreatePbufferSurface(eglDisplay, eglConfig, surfaceAttributes, 0);
-
-        EGL14.eglMakeCurrent(eglDisplay, eglSurface, eglSurface, eglContext);
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    // EGL destroy
-    private void release() {
-        EGL14.eglMakeCurrent(eglDisplay, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_SURFACE, EGL14.EGL_NO_CONTEXT);
-        EGL14.eglDestroySurface(eglDisplay, eglSurface);
-        EGL14.eglDestroyContext(eglDisplay, eglContext);
-        EGL14.eglTerminate(eglDisplay);
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
     }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation 'androidx.appcompat:appcompat:1.6.1'
+    implementation 'com.google.android.material:material:1.5.0'
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
 }
